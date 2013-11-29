b1ad5efc0b4eee85a8d82b3dee90a4eb0b438357
Fabrice Nourisson <fnourisson@servebox.com>
2013-11-29 12:14:04 +0100

    Refactor tests

--- spec/metadata/handler_spec.rb
+++ spec/metadata/handler_spec.rb
@@ -1,18 +1,21 @@
 require 'spec_helper'
 
-describe Jobz::Metadata::Handler do
-    
+describe Jobz::Metadata::Handler do    
     subject do
         Jobz::Metadata::Handler.new :foo
     end
 
-    class Mock
-        def id
-            :bar
-        end
+    it 'should add the document id to the metadata' do
+        model = Object.new
+        model.class.expects(:name).returns('Model')
+        model.expects(:id).returns(1)
+        subject.metadata(model).must_equal({class_name: 'Model', foo: 1})
     end
 
-    it 'should extract class name' do
-        subject.metadata(Mock.new).must_equal({class_name: 'Mock', foo: :bar})
+    it 'should resolve object using Mongoid finder' do
+        klazz = Class.new
+        model = klazz.new.tap{|o| o.stubs(:id).returns(1)}
+        klazz.expects(:find).with(1).returns(model)
+        subject.resolve(klazz, {foo: 1}).id.must_equal 1
     end
 end
--- spec/metadata/mongoid_spec.rb
+++ spec/metadata/mongoid_spec.rb
@@ -6,29 +6,8 @@ describe Jobz::Metadata::Mongoid do
         Class.new.tap{ |klazz| klazz.send(:include, Jobz::Metadata::Mongoid) }
     end
 
-    it 'should return an handler for active record' do
-        subject.jobz.must_be_instance_of Jobz::Metadata::Handler
-    end
-end
-
-
-describe Jobz::Metadata::Handler do
-    
-    subject do
-        Jobz::Metadata::Handler.new :document_id
-    end
-
-    it 'should add the document id to the metadata' do
-        model = Object.new
-        model.class.expects(:name).returns('Model')
-        model.expects(:id).returns(1)
-        subject.metadata(model).must_equal({class_name: 'Model', document_id: 1})
-    end
-
-    it 'should resolve object using Mongoid finder' do
-        klazz = Class.new
-        model = klazz.new.tap{|o| o.stubs(:id).returns(1)}
-        klazz.expects(:find).with(1).returns(model)
-        subject.resolve(klazz, {document_id: 1}).id.must_equal 1
+    it 'should return an handler for mongoid' do
+        Jobz::Metadata::Handler.expects(:new).with(:document_id).returns(:foo)
+        subject.jobz.must_equal :foo
     end
 end
